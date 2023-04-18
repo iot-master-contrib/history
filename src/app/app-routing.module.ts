dcd113462fcccf1d9904fc5cdd581c8c4e161b2b
T-r-ee <1311337083@qq.com>
2023-04-18 18:06:27 +0800

    history

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HistorysComponent } from './history/historys/historys.component';
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/alarm/alarms' },
+  { path: 'history/historys', component: HistorysComponent },
+  { path: '**', component: PageNotFoundComponent },
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,51 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { zh_CN } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { HistorysComponent } from './history/historys/historys.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NzTableModule } from 'ng-zorro-antd/table'; 
+import { NzDividerModule } from 'ng-zorro-antd/divider'; 
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NzModalModule } from 'ng-zorro-antd/modal'; 
+import { NzInputModule } from 'ng-zorro-antd/input';
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HistorysComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NzButtonModule,
+    NzDividerModule,
+    NzTableModule ,
+    NzSpaceModule,
+    NzMessageModule,
+    NzIconModule,
+    AppRoutingModule,
+    NzSpinModule,
+    NzInputModule,
+    NzModalModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: zh_CN }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
