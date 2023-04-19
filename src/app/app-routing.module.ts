91c18bc0fdf8ae268991d71d22dd81fb8c9e11b4
Jason <jason@zgwit.com>
2023-04-19 12:11:30 +0800

    项目结构调整

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router'; 
+import { RouterModule, Routes } from '@angular/router';
 import { HistorysComponent } from './history/historys/historys.component';
-import { JobEditComponent } from './history/job/job-edit/job-edit.component';
-import { JobsComponent } from './history/job/jobs/jobs.component';
+import { JobEditComponent } from './job/job-edit/job-edit.component';
+import { JobsComponent } from './job/jobs/jobs.component';
 import { PageNotFoundComponent } from './base/page-not-found/page-not-found.component';
 const routes: Routes = [
-  
+
  { path: '', pathMatch: 'full', redirectTo: 'history' },
     { path: 'history', component: HistorysComponent },
     { path: 'job', component: JobsComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,21 @@ import { AppComponent } from './app.component';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { zh_CN } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
-import zh from '@angular/common/locales/zh'; 
+import zh from '@angular/common/locales/zh';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NzButtonModule } from 'ng-zorro-antd/button';
-import { HistorysComponent } from './history/historys/historys.component'; 
-import { NzTableModule } from 'ng-zorro-antd/table'; 
-import { NzDividerModule } from 'ng-zorro-antd/divider'; 
+import { HistorysComponent } from './history/historys/historys.component';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzSpaceModule } from 'ng-zorro-antd/space';
 import { NzSpinModule } from 'ng-zorro-antd/spin';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzMessageModule } from 'ng-zorro-antd/message';
-import { NzModalModule } from 'ng-zorro-antd/modal'; 
+import { NzModalModule } from 'ng-zorro-antd/modal';
 import { NzInputModule } from 'ng-zorro-antd/input';
-import { JobsComponent } from './history/job/jobs/jobs.component';
-import { JobEditComponent } from './history/job/job-edit/job-edit.component';
+import { JobsComponent } from './job/jobs/jobs.component';
+import { JobEditComponent } from './job/job-edit/job-edit.component';
 import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzSelectModule } from 'ng-zorro-antd/select';
@@ -28,13 +28,13 @@ import { NzTagModule } from 'ng-zorro-antd/tag';
 import {APP_BASE_HREF } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {BaseModule} from "./base/base.module";
-import {NzCardModule} from "ng-zorro-antd/card"; 
+import {NzCardModule} from "ng-zorro-antd/card";
 registerLocaleData(zh);
 
 @NgModule({
   declarations: [
     AppComponent,
-    HistorysComponent, 
+    HistorysComponent,
     JobsComponent,
     JobEditComponent
   ],
